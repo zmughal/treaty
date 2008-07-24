383340c77a8785f2682113f709dc0feedaae17b3
jens.dietrich <jens.dietrich@a4919d77-c54b-0410-8eaf-491ad53b66f1>
2008-07-24 10:41:12 +0000

    Initial import.

---
+++ treaty/src/net/java/treaty/Connector.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2008 Jens Dietrich
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License 
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and limitations under the License. 
+ */
+
+package net.java.treaty;
+
+/**
+ * This interface abstracts from extension points (consuming) and extensions (providing).  
+ * @author Jens Dietrich
+ */
+
+public interface Connector<C extends Component> {
+	/**
+	 * Indicates the connector type.
+	 * @return
+	 */
+	ConnectorType getType();
+	/**
+	 * Get the connector id.
+	 * For instance, this could be the id of the eclipse extension point or extension.
+	 * @return
+	 */
+	String getId();
+	/**
+	 * Get the component that owns this connector.
+	 * @return
+	 */
+	C getOwner();
+	/**
+	 * Get the parameter value. For instance, a component meta data property file might have 
+	 * an entry class=com.example.MyClass. Then getParameter("class") should then return "com.example.MyClass". 
+	 * @param paramName
+	 * @return
+	 */
+	String getParameter(String paramName);
+}
---
+++ treaty/src/net/java/treaty/verification/VerificationResultPrinter.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2008 Jens Dietrich
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License 
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and limitations under the License. 
+ */
+
+package net.java.treaty.verification;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import net.java.treaty.SimpleContract;
+
+/**
+ * Used to export verification results into XML files.
+ * @author Jens Dietrich
+ */
+
+public class VerificationResultPrinter {
+	
+	public String print(Map<SimpleContract,VerificationReport> results) throws IOException {
+		String location = new File("verificationresults.xml").toURI().toString();
+		PrintStream out = new PrintStream(new FileOutputStream("verificationresults.xml"));
+		String root = new File(".").toURL().toString();
+		
+		out.println("<?xml version=\"1.0\" ?>");
+		out.println("<?xml-stylesheet type=\"text/xsl\" href=\"verificationresults.xsl\"?>");
+		out.println("<verificationresults>");
+		for (Map.Entry<SimpleContract,VerificationReport> result:results.entrySet()) {
+			SimpleContract contract = result.getKey();
+			Collection<VerificationReport.LogEntry> entries = result.getValue().getEntries();
+			Map<Object,VerificationReport.LogEntry> entriesByObject = new HashMap<Object,VerificationReport.LogEntry>();
+			for (VerificationReport.LogEntry entry:entries)
+				entriesByObject.put(entry.getContext(),entry);
+			VerificationResultVisitor visitor = new VerificationResultVisitor(out,entriesByObject,root);
+			contract.accept(visitor);
+		}
+		out.println("</verificationresults>");
+		out.close();
+		return location;
+	}
+
+}
