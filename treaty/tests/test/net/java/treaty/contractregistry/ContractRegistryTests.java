c43e199733bbf2dcdc1fca788b7e6a672b5a0fe7
claaswilke <claaswilke@gmx.net>
2009-11-03 01:36:34 +0000

    Implemented method ContractRegistry.getAffectedContracts(LifeCycleEvent).

--- treaty/tests/test/net/java/treaty/contractregistry/ContractRegistryTests.java
+++ treaty/tests/test/net/java/treaty/contractregistry/ContractRegistryTests.java
@@ -21,6 +21,7 @@ import net.java.treaty.Role;
 import net.java.treaty.TreatyException;
 import net.java.treaty.contractregistry.ContractRegistry;
 import net.java.treaty.contractregistry.ContractRegistry.UpdateType;
+import net.java.treaty.event.LifeCycleEvent;
 
 import org.junit.Test;
 
@@ -679,6 +680,501 @@ public class ContractRegistryTests {
 		assertEquals(0, contractRegistry.getContracts(supplierMock, Role.CONSUMER)
 				.size());
 		assertEquals(0, contractRegistry.getContracts(supplierMock, Role.SUPPLIER)
+				.size());
+	}
+
+	/**
+	 * <p>
+	 * Tests the method
+	 * {@link ContractRegistry#getAffectedContracts(net.java.treaty.event.LifeCycleEvent)}
+	 * .
+	 * </p>
+	 */
+	@Test
+	public void testGetAffectedContracts01() {
+
+		ComponentMock consumerComponentMock;
+		consumerComponentMock = new ComponentMock("Consumer Component 1");
+
+		ComponentMock supplier1ComponentMock;
+		supplier1ComponentMock = new ComponentMock("Supplier Component 1");
+
+		ComponentMock supplier2ComponentMock;
+		supplier2ComponentMock = new ComponentMock("Supplier Component 2");
+
+		ConnectorMock consumerMock;
+		consumerMock =
+				new ConnectorMock("Consumer 1", consumerComponentMock,
+						ConnectorType.CONSUMER);
+
+		ConnectorMock supplierMock1;
+		supplierMock1 =
+				new ConnectorMock("Supplier 1", supplier1ComponentMock,
+						ConnectorType.SUPPLIER);
+
+		ConnectorMock supplierMock2;
+		supplierMock2 =
+				new ConnectorMock("Supplier 2", supplier2ComponentMock,
+						ConnectorType.SUPPLIER);
+
+		Contract contract;
+		contract = new Contract();
+
+		ContractRegistry contractRegistry;
+		contractRegistry = new ContractRegistry();
+
+		LifeCycleEvent consumerUpdate;
+		consumerUpdate = new LifeCycleEvent(consumerComponentMock, null);
+
+		LifeCycleEvent supplier1Update;
+		supplier1Update = new LifeCycleEvent(supplier1ComponentMock, null);
+
+		LifeCycleEvent supplier2Update;
+		supplier2Update = new LifeCycleEvent(supplier2ComponentMock, null);
+
+		/* Add the consumer contract. */
+		try {
+			contractRegistry.updateContract(UpdateType.ADD_CONTRACT, contract,
+					consumerMock, Role.CONSUMER);
+		}
+
+		catch (TreatyException e) {
+			fail("Unexpected TreatyException: " + e.getMessage());
+		}
+		// end catch.
+
+		assertEquals(0, contractRegistry.getAffectedContracts(consumerUpdate)
+				.size());
+		assertEquals(0, contractRegistry.getAffectedContracts(supplier1Update)
+				.size());
+		assertEquals(0, contractRegistry.getAffectedContracts(supplier2Update)
+				.size());
+
+		/* Add a supplier contract. */
+		try {
+			contractRegistry.updateContract(UpdateType.ADD_CONTRACT, contract,
+					supplierMock1, Role.SUPPLIER);
+		}
+
+		catch (TreatyException e) {
+			fail("Unexpected TreatyException: " + e.getMessage());
+		}
+		// end catch.
+
+		assertEquals(1, contractRegistry.getAffectedContracts(consumerUpdate)
+				.size());
+		assertEquals(1, contractRegistry.getAffectedContracts(supplier1Update)
+				.size());
+		assertEquals(0, contractRegistry.getAffectedContracts(supplier2Update)
+				.size());
+
+		/* Add a supplier contract. */
+		try {
+			contractRegistry.updateContract(UpdateType.ADD_CONTRACT, contract,
+					supplierMock2, Role.SUPPLIER);
+		}
+
+		catch (TreatyException e) {
+			fail("Unexpected TreatyException: " + e.getMessage());
+		}
+		// end catch.
+
+		assertEquals(2, contractRegistry.getAffectedContracts(consumerUpdate)
+				.size());
+		assertEquals(1, contractRegistry.getAffectedContracts(supplier1Update)
+				.size());
+		assertEquals(1, contractRegistry.getAffectedContracts(supplier2Update)
+				.size());
+	}
+
+	/**
+	 * <p>
+	 * Tests the method
+	 * {@link ContractRegistry#getAffectedContracts(net.java.treaty.event.LifeCycleEvent)}
+	 * .
+	 * </p>
+	 */
+	@Test
+	public void testGetAffectedContracts02() {
+
+		ComponentMock consumer1ComponentMock;
+		consumer1ComponentMock = new ComponentMock("Consumer Component 1");
+
+		ComponentMock consumer2ComponentMock;
+		consumer2ComponentMock = new ComponentMock("Consumer Component 2");
+
+		ComponentMock supplier1ComponentMock;
+		supplier1ComponentMock = new ComponentMock("Supplier Component 1");
+
+		ConnectorMock consumerMock1;
+		consumerMock1 =
+				new ConnectorMock("Consumer 1", consumer1ComponentMock,
+						ConnectorType.CONSUMER);
+
+		ConnectorMock consumerMock2;
+		consumerMock2 =
+				new ConnectorMock("Consumer 2", consumer2ComponentMock,
+						ConnectorType.CONSUMER);
+
+		ConnectorMock supplierMock1;
+		supplierMock1 =
+				new ConnectorMock("Supplier 1", supplier1ComponentMock,
+						ConnectorType.SUPPLIER);
+
+		Contract contract;
+		contract = new Contract();
+
+		ContractRegistry contractRegistry;
+		contractRegistry = new ContractRegistry();
+
+		LifeCycleEvent consumer1Update;
+		consumer1Update = new LifeCycleEvent(consumer1ComponentMock, null);
+
+		LifeCycleEvent consumer2Update;
+		consumer2Update = new LifeCycleEvent(consumer2ComponentMock, null);
+
+		LifeCycleEvent supplier1Update;
+		supplier1Update = new LifeCycleEvent(supplier1ComponentMock, null);
+
+		/* Add the supplier contract. */
+		try {
+			contractRegistry.updateContract(UpdateType.ADD_CONTRACT, contract,
+					supplierMock1, Role.SUPPLIER);
+		}
+
+		catch (TreatyException e) {
+			fail("Unexpected TreatyException: " + e.getMessage());
+		}
+		// end catch.
+
+		assertEquals(0, contractRegistry.getAffectedContracts(consumer1Update)
+				.size());
+		assertEquals(0, contractRegistry.getAffectedContracts(supplier1Update)
+				.size());
+		assertEquals(0, contractRegistry.getAffectedContracts(consumer2Update)
+				.size());
+
+		/* Add a consumer contract. */
+		try {
+			contractRegistry.updateContract(UpdateType.ADD_CONTRACT, contract,
+					consumerMock1, Role.CONSUMER);
+		}
+
+		catch (TreatyException e) {
+			fail("Unexpected TreatyException: " + e.getMessage());
+		}
+		// end catch.
+
+		assertEquals(1, contractRegistry.getAffectedContracts(consumer1Update)
+				.size());
+		assertEquals(1, contractRegistry.getAffectedContracts(supplier1Update)
+				.size());
+		assertEquals(0, contractRegistry.getAffectedContracts(consumer2Update)
+				.size());
+
+		/* Add a consumer contract. */
+		try {
+			contractRegistry.updateContract(UpdateType.ADD_CONTRACT, contract,
+					consumerMock2, Role.CONSUMER);
+		}
+
+		catch (TreatyException e) {
+			fail("Unexpected TreatyException: " + e.getMessage());
+		}
+		// end catch.
+
+		assertEquals(1, contractRegistry.getAffectedContracts(consumer1Update)
+				.size());
+		assertEquals(2, contractRegistry.getAffectedContracts(supplier1Update)
+				.size());
+		assertEquals(1, contractRegistry.getAffectedContracts(consumer2Update)
+				.size());
+	}
+
+	/**
+	 * <p>
+	 * Tests the method
+	 * {@link ContractRegistry#getAffectedContracts(net.java.treaty.event.LifeCycleEvent)}
+	 * .
+	 * </p>
+	 */
+	@Test
+	public void testGetAffectedContracts03() {
+
+		ComponentMock legislator1ComponentMock;
+		legislator1ComponentMock = new ComponentMock("Legislator Component 1");
+
+		ComponentMock consumer1ComponentMock;
+		consumer1ComponentMock = new ComponentMock("Consumer Component 1");
+
+		ComponentMock supplier1ComponentMock;
+		supplier1ComponentMock = new ComponentMock("Supplier Component 1");
+
+		ComponentMock supplier2ComponentMock;
+		supplier2ComponentMock = new ComponentMock("Supplier Component 2");
+
+		ConnectorMock legislator1Mock;
+		legislator1Mock =
+				new ConnectorMock("Legislator 1", legislator1ComponentMock,
+						ConnectorType.CONSUMER);
+
+		ConnectorMock consumer1Mock;
+		consumer1Mock =
+				new ConnectorMock("Consumer 1", consumer1ComponentMock,
+						ConnectorType.CONSUMER);
+
+		ConnectorMock supplierMock1;
+		supplierMock1 =
+				new ConnectorMock("Supplier 1", supplier1ComponentMock,
+						ConnectorType.SUPPLIER);
+
+		ConnectorMock supplierMock2;
+		supplierMock2 =
+				new ConnectorMock("Supplier 2", supplier2ComponentMock,
+						ConnectorType.SUPPLIER);
+
+		Contract contract;
+		contract = new Contract();
+
+		ContractRegistry contractRegistry;
+		contractRegistry = new ContractRegistry();
+
+		LifeCycleEvent legislator1Update;
+		legislator1Update = new LifeCycleEvent(legislator1ComponentMock, null);
+
+		LifeCycleEvent consumer1Update;
+		consumer1Update = new LifeCycleEvent(consumer1ComponentMock, null);
+
+		LifeCycleEvent supplier1Update;
+		supplier1Update = new LifeCycleEvent(supplier1ComponentMock, null);
+
+		LifeCycleEvent supplier2Update;
+		supplier2Update = new LifeCycleEvent(supplier2ComponentMock, null);
+
+		/* Add the legislator contract. */
+		try {
+			contractRegistry.updateContract(UpdateType.ADD_CONTRACT, contract,
+					legislator1Mock, Role.LEGISLATOR);
+		}
+
+		catch (TreatyException e) {
+			fail("Unexpected TreatyException: " + e.getMessage());
+		}
+		// end catch.
+
+		assertEquals(0, contractRegistry.getAffectedContracts(legislator1Update)
+				.size());
+		assertEquals(0, contractRegistry.getAffectedContracts(consumer1Update)
+				.size());
+		assertEquals(0, contractRegistry.getAffectedContracts(supplier1Update)
+				.size());
+		assertEquals(0, contractRegistry.getAffectedContracts(supplier2Update)
+				.size());
+
+		/* Add the consumer contract. */
+		try {
+			contractRegistry.updateContract(UpdateType.ADD_CONTRACT, contract,
+					consumer1Mock, Role.CONSUMER);
+		}
+
+		catch (TreatyException e) {
+			fail("Unexpected TreatyException: " + e.getMessage());
+		}
+		// end catch.
+
+		assertEquals(0, contractRegistry.getAffectedContracts(legislator1Update)
+				.size());
+		assertEquals(0, contractRegistry.getAffectedContracts(consumer1Update)
+				.size());
+		assertEquals(0, contractRegistry.getAffectedContracts(supplier1Update)
+				.size());
+		assertEquals(0, contractRegistry.getAffectedContracts(supplier2Update)
+				.size());
+
+		/* Add a supplier contract. */
+		try {
+			contractRegistry.updateContract(UpdateType.ADD_CONTRACT, contractRegistry
+					.getContracts(consumer1Mock, Role.CONSUMER).iterator().next(),
+					supplierMock1, Role.SUPPLIER);
+		}
+
+		catch (TreatyException e) {
+			fail("Unexpected TreatyException: " + e.getMessage());
+		}
+		// end catch.
+
+		assertEquals(0, contractRegistry.getAffectedContracts(legislator1Update)
+				.size());
+		assertEquals(1, contractRegistry.getAffectedContracts(consumer1Update)
+				.size());
+		assertEquals(1, contractRegistry.getAffectedContracts(supplier1Update)
+				.size());
+		assertEquals(0, contractRegistry.getAffectedContracts(supplier2Update)
+				.size());
+
+		/* Add a supplier contract. */
+		try {
+			contractRegistry.updateContract(UpdateType.ADD_CONTRACT, contractRegistry
+					.getContracts(consumer1Mock, Role.CONSUMER).iterator().next(),
+					supplierMock2, Role.SUPPLIER);
+		}
+
+		catch (TreatyException e) {
+			fail("Unexpected TreatyException: " + e.getMessage());
+		}
+		// end catch.
+
+		assertEquals(0, contractRegistry.getAffectedContracts(legislator1Update)
+				.size());
+		assertEquals(2, contractRegistry.getAffectedContracts(consumer1Update)
+				.size());
+		assertEquals(1, contractRegistry.getAffectedContracts(supplier1Update)
+				.size());
+		assertEquals(1, contractRegistry.getAffectedContracts(supplier2Update)
+				.size());
+
+	}
+
+	/**
+	 * <p>
+	 * Tests the method
+	 * {@link ContractRegistry#getAffectedContracts(net.java.treaty.event.LifeCycleEvent)}
+	 * .
+	 * </p>
+	 */
+	@Test
+	public void testGetAffectedContracts04() {
+
+		ComponentMock legislator1ComponentMock;
+		legislator1ComponentMock = new ComponentMock("Legislator Component 1");
+
+		ComponentMock consumer1ComponentMock;
+		consumer1ComponentMock = new ComponentMock("Consumer Component 1");
+
+		ComponentMock consumer2ComponentMock;
+		consumer2ComponentMock = new ComponentMock("Consumer Component 2");
+
+		ComponentMock supplier1ComponentMock;
+		supplier1ComponentMock = new ComponentMock("Supplier Component 1");
+
+		ConnectorMock legislator1Mock;
+		legislator1Mock =
+				new ConnectorMock("Legislator 1", legislator1ComponentMock,
+						ConnectorType.CONSUMER);
+
+		ConnectorMock consumerMock1;
+		consumerMock1 =
+				new ConnectorMock("Consumer 1", consumer1ComponentMock,
+						ConnectorType.CONSUMER);
+
+		ConnectorMock consumerMock2;
+		consumerMock2 =
+				new ConnectorMock("Consumer 2", consumer2ComponentMock,
+						ConnectorType.CONSUMER);
+
+		ConnectorMock supplierMock1;
+		supplierMock1 =
+				new ConnectorMock("Supplier 1", supplier1ComponentMock,
+						ConnectorType.SUPPLIER);
+
+		Contract contract;
+		contract = new Contract();
+
+		ContractRegistry contractRegistry;
+		contractRegistry = new ContractRegistry();
+
+		LifeCycleEvent legislator1Update;
+		legislator1Update = new LifeCycleEvent(legislator1ComponentMock, null);
+
+		LifeCycleEvent consumer1Update;
+		consumer1Update = new LifeCycleEvent(consumer1ComponentMock, null);
+
+		LifeCycleEvent consumer2Update;
+		consumer2Update = new LifeCycleEvent(consumer2ComponentMock, null);
+
+		LifeCycleEvent supplier1Update;
+		supplier1Update = new LifeCycleEvent(supplier1ComponentMock, null);
+
+		/* Add the legislator contract. */
+		try {
+			contractRegistry.updateContract(UpdateType.ADD_CONTRACT, contract,
+					legislator1Mock, Role.LEGISLATOR);
+		}
+
+		catch (TreatyException e) {
+			fail("Unexpected TreatyException: " + e.getMessage());
+		}
+		// end catch.
+
+		assertEquals(0, contractRegistry.getAffectedContracts(legislator1Update)
+				.size());
+		assertEquals(0, contractRegistry.getAffectedContracts(supplier1Update)
+				.size());
+		assertEquals(0, contractRegistry.getAffectedContracts(consumer1Update)
+				.size());
+		assertEquals(0, contractRegistry.getAffectedContracts(consumer2Update)
+				.size());
+
+		/* Add the supplier contract. */
+		try {
+			contractRegistry.updateContract(UpdateType.ADD_CONTRACT, contract,
+					supplierMock1, Role.SUPPLIER);
+		}
+
+		catch (TreatyException e) {
+			fail("Unexpected TreatyException: " + e.getMessage());
+		}
+		// end catch.
+
+		assertEquals(0, contractRegistry.getAffectedContracts(legislator1Update)
+				.size());
+		assertEquals(0, contractRegistry.getAffectedContracts(supplier1Update)
+				.size());
+		assertEquals(0, contractRegistry.getAffectedContracts(consumer1Update)
+				.size());
+		assertEquals(0, contractRegistry.getAffectedContracts(consumer2Update)
+				.size());
+
+		/* Add a consumer contract. */
+		try {
+			contractRegistry.updateContract(UpdateType.ADD_CONTRACT, contractRegistry
+					.getContracts(supplierMock1, Role.SUPPLIER).iterator().next(),
+					consumerMock1, Role.CONSUMER);
+		}
+
+		catch (TreatyException e) {
+			fail("Unexpected TreatyException: " + e.getMessage());
+		}
+		// end catch.
+
+		assertEquals(0, contractRegistry.getAffectedContracts(legislator1Update)
+				.size());
+		assertEquals(1, contractRegistry.getAffectedContracts(supplier1Update)
+				.size());
+		assertEquals(1, contractRegistry.getAffectedContracts(consumer1Update)
+				.size());
+		assertEquals(0, contractRegistry.getAffectedContracts(consumer2Update)
+				.size());
+
+		/* Add a consumer contract. */
+		try {
+			contractRegistry.updateContract(UpdateType.ADD_CONTRACT, contractRegistry
+					.getContracts(supplierMock1, Role.SUPPLIER).iterator().next(),
+					consumerMock2, Role.CONSUMER);
+		}
+
+		catch (TreatyException e) {
+			fail("Unexpected TreatyException: " + e.getMessage());
+		}
+		// end catch.
+
+		assertEquals(0, contractRegistry.getAffectedContracts(legislator1Update)
+				.size());
+		assertEquals(2, contractRegistry.getAffectedContracts(supplier1Update)
+				.size());
+		assertEquals(1, contractRegistry.getAffectedContracts(consumer1Update)
+				.size());
+		assertEquals(1, contractRegistry.getAffectedContracts(consumer2Update)
 				.size());
 	}
 }
