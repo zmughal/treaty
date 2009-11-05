9f74a1c765dceac126b33a6e560b33332305c73f
jens.dietrich <jens.dietrich@gmail.com>
2009-11-05 02:03:24 +0000

    added support for built-in operators

--- treaty/src/net/java/treaty/vocabulary/builtins/BuiltInOperator.java
+++ treaty/src/net/java/treaty/vocabulary/builtins/BuiltInOperator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008 Jens Dietrich
+ * Copyright (C) 2009 Jens Dietrich
  * Licensed under the Apache License, Version 2.0 (the "License"); 
  * you may not use this file except in compliance with the License. 
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 
@@ -8,8 +8,10 @@
  * See the License for the specific language governing permissions and limitations under the License. 
  */
 
-package net.java.treaty;
+package net.java.treaty.vocabulary.builtins;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.StringTokenizer;
 import java.util.regex.Pattern;
 
@@ -19,27 +21,36 @@ import java.util.regex.Pattern;
  * @author Jens Dietrich
  */
 
-public abstract class Operator {
+public abstract class BuiltInOperator {
 	
+	public static final String OP = "http://www.treaty.org/builtin/#";
 
 	
-	static class EQ extends Operator {
+	static class EQ extends BuiltInOperator {
 		public boolean compare(Object o1,Object o2) {
 			return o1==null?o2==null:o1.equals(o2);
 		}
 		public String getName() {
 			return "=";
 		}
+		private URI uri = buildURI("eq");
+		public URI getURI() {
+			return uri;
+		}
 	};
-	static class NEQ extends Operator {
+	static class NEQ extends BuiltInOperator {
 		public boolean compare(Object o1,Object o2) {
 			return o1==null?o2!=null:!o1.equals(o2);
 		}
 		public String getName() {
 			return "!=";
 		}
+		private URI uri = buildURI("neq");
+		public URI getURI() {
+			return uri;
+		}
 	};
-	static class IN extends Operator {
+	static class IN extends BuiltInOperator {
 		public boolean compare(Object o1,Object o2) {
 			if (o1==null) return false;
 			if (o2==null) return false;
@@ -54,9 +65,13 @@ public abstract class Operator {
 		public String getName() {
 			return "IN";
 		}
+		private URI uri = buildURI("in");
+		public URI getURI() {
+			return uri;
+		}
 	};
 	
-	static class REGEX extends Operator {
+	static class REGEX extends BuiltInOperator {
 		public boolean compare(Object o1,Object o2) {
 			if (!(o1 instanceof String)) {
 				throw new IllegalArgumentException("the first parameter must be a string");
@@ -70,9 +85,13 @@ public abstract class Operator {
 		public String getName() {
 			return "matches";
 		}
+		private URI uri = buildURI("matches");
+		public URI getURI() {
+			return uri;
+		}
 	};
 	
-	static class LT extends Operator {
+	static class LT extends BuiltInOperator {
 		public boolean compare(Object o1,Object o2) {
 			double d1 = toDouble(o1);
 			double d2 = toDouble(o2);
@@ -81,8 +100,12 @@ public abstract class Operator {
 		public String getName() {
 			return "lt";
 		}
+		private URI uri = buildURI("lt");
+		public URI getURI() {
+			return uri;
+		}
 	};
-	static class LTE extends Operator {
+	static class LTE extends BuiltInOperator {
 		public boolean compare(Object o1,Object o2) {
 			double d1 = toDouble(o1);
 			double d2 = toDouble(o2);
@@ -91,8 +114,12 @@ public abstract class Operator {
 		public String getName() {
 			return "lte";
 		}
+		private URI uri = buildURI("lte");
+		public URI getURI() {
+			return uri;
+		}
 	};
-	static class GT extends Operator {
+	static class GT extends BuiltInOperator {
 		public boolean compare(Object o1,Object o2) {
 			double d1 = toDouble(o1);
 			double d2 = toDouble(o2);
@@ -101,8 +128,12 @@ public abstract class Operator {
 		public String getName() {
 			return "gt";
 		}
+		private URI uri = buildURI("gt");
+		public URI getURI() {
+			return uri;
+		}
 	};
-	static class GTE extends Operator {
+	static class GTE extends BuiltInOperator {
 		public boolean compare(Object o1,Object o2) {
 			double d1 = toDouble(o1);
 			double d2 = toDouble(o2);
@@ -111,24 +142,15 @@ public abstract class Operator {
 		public String getName() {
 			return "gte";
 		}
-	};
-	
-	public static Operator[] INSTANCES = {
-			new EQ(),new NEQ(),new IN(), new REGEX(), new LT(), new GT(), new LTE(), new GTE()	
-		};
-		
-		public static Operator getInstance(String name) {
-			for (Operator o:INSTANCES) {
-				if (o.getName().equalsIgnoreCase(name)) {
-					return o;
-				}
-			}
-			return null;
+		private URI uri = buildURI("lte");
+		public URI getURI() {
+			return uri;
 		}
-		
+	};
 	
 	public abstract boolean compare(Object o1,Object o2);
 	public abstract String getName();
+	public abstract URI getURI();
 	
 	private static double toDouble(Object obj) {
 		if (obj instanceof String) {
@@ -143,4 +165,12 @@ public abstract class Operator {
 		else throw new IllegalArgumentException("double parameter expected here");
 	}
 	
+	private static URI buildURI(String string) {
+		try {
+			return new URI(OP+string);
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
