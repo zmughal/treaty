bf63a4d062002492ef8685d2d47d546327906db1
claaswilke <claaswilke@gmx.net>
2009-11-09 23:55:48 +0000

    Adapted the structure of the XML vocabulary to the new ContractVocabulary interface.

--- treaty-eclipse-voc-xml/src/net/java/treaty/eclipse/vocabulary/xml/Activator.java
+++ treaty-eclipse-voc-xml/src/net/java/treaty/eclipse/vocabulary/xml/Activator.java
@@ -1,5 +1,7 @@
 package net.java.treaty.eclipse.vocabulary.xml;
 
+import net.java.treaty.eclipse.VocabularyRegistry;
+
 import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
 
@@ -9,41 +11,57 @@ import org.osgi.framework.BundleContext;
 public class Activator extends Plugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "net.java.treaty.eclipse.vocabulary.xml";
+	public static final String PLUGIN_ID =
+			"net.java.treaty.eclipse.vocabulary.xml";
 
 	// The shared instance
 	private static Activator plugin;
-	
+
 	/**
 	 * The constructor
 	 */
 	public Activator() {
+
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void start(BundleContext context) throws Exception {
+
 		super.start(context);
 		plugin = this;
+
+		/* Register the vocabulary add the VocabularyRegistry. */
+		VocabularyRegistry.INSTANCE
+				.add(XMLVocabulary.INSTANCE, Activator.PLUGIN_ID);
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void stop(BundleContext context) throws Exception {
+
 		plugin = null;
 		super.stop(context);
+
+		/* Register the vocabulary add the VocabularyRegistry. */
+		VocabularyRegistry.INSTANCE.remove(XMLVocabulary.INSTANCE);
 	}
 
 	/**
 	 * Returns the shared instance
-	 *
+	 * 
 	 * @return the shared instance
 	 */
 	public static Activator getDefault() {
+
 		return plugin;
 	}
 
