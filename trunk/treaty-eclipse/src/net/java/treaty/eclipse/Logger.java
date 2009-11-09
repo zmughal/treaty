cd20888399d2a33066e2155cfbb16e246766d15e
claaswilke@gmx.net <claaswilke@gmx.net@a4919d77-c54b-0410-8eaf-491ad53b66f1>
2009-11-09 02:22:13 +0000

    Refactorings and Java-doc.

--- trunk/treaty-eclipse/src/net/java/treaty/eclipse/Logger.java
+++ trunk/treaty-eclipse/src/net/java/treaty/eclipse/Logger.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008 Jens Dietrich
+ * Copyright (C) 2009 Jens Dietrich
  * Licensed under the Apache License, Version 2.0 (the "License"); 
  * you may not use this file except in compliance with the License. 
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 
@@ -12,37 +12,114 @@ package net.java.treaty.eclipse;
 
 import org.eclipse.core.runtime.Status;
 
-
 /**
- * Log utility.
+ * <p>
+ * {@link Logger} utility for the Eclipse Treaty implementation.
+ * </p>
+ * 
  * @author Jens Dietrich
  */
-
 public class Logger {
-	   public static void info(String msg) {
-		      info(msg, null);
-	   }
-	   public static void info(String msg, Throwable e) {
-	      log(msg,e,Status.INFO);
-	   }
-	   
-	   public static void warn(String msg) {
-		      warn(msg, null);
-	   }
-	   public static void warn(String msg, Throwable e) {
-	      log(msg,e,Status.WARNING);
-	   }
-	   
-	   public static void error(String msg) {
-		      error(msg, null);
-	   }
-	   public static void error(String msg, Throwable e) {
-	      log(msg,e,Status.ERROR);
-	   }
-	   
-	   public static void log(String msg, Throwable e,int status) {
-		      Activator.getDefault().getLog().log(new Status(status, Activator.PLUGIN_ID, Status.OK, msg, e));
-	   }
 
+	/**
+	 * <p>
+	 * Logs an error.
+	 * </p>
+	 * 
+	 * @param msg
+	 *          The error to be logged.
+	 */
+	public static void error(String msg) {
 
+		error(msg, null);
+	}
+
+	/**
+	 * <p>
+	 * Logs an error.
+	 * </p>
+	 * 
+	 * @param msg
+	 *          The error to be logged.
+	 * @param e
+	 *          The {@link Throwable} to be logged.
+	 */
+	public static void error(String msg, Throwable e) {
+
+		log(msg, e, Status.ERROR);
+	}
+
+	/**
+	 * <p>
+	 * Logs an info message.
+	 * </p>
+	 * 
+	 * @param msg
+	 *          The info message to be logged.
+	 */
+	public static void info(String msg) {
+
+		info(msg, null);
+	}
+
+	/**
+	 * <p>
+	 * Logs an info message for a given {@link Throwable}.
+	 * </p>
+	 * 
+	 * @param msg
+	 *          The info message to be logged.
+	 * @param e
+	 *          The {@link Throwable} to be logged.
+	 */
+	public static void info(String msg, Throwable e) {
+
+		log(msg, e, Status.INFO);
+	}
+
+	/**
+	 * <p>
+	 * Logs a given message for a given {@link Throwable} and a given status.
+	 * </p>
+	 * 
+	 * @param msg
+	 *          The message to be logged.
+	 * @param e
+	 *          The {@link Throwable} to be logged.
+	 * @param status
+	 *          The status to be logged.
+	 */
+	public static void log(String msg, Throwable e, int status) {
+
+		Activator.getDefault().getLog().log(
+				new Status(status, Activator.PLUGIN_ID, Status.OK, msg, e));
+	}
+
+	/**
+	 * <p>
+	 * Logs a warning.
+	 * </p>
+	 * 
+	 * @param msg
+	 *          The warning to be logged.
+	 */
+	public static void warn(String msg) {
+
+		warn(msg, null);
+	}
+
+	/**
+	 * <p>
+	 * Logs a warning for a given {@link Throwable}.
+	 * </p>
+	 * 
+	 * @param msg
+	 *          The warning to be logged.
+	 * @param e
+	 *          The {@link Throwable} to be logged.
+	 */
+	public static void warn(String msg, Throwable e) {
+
+		log(msg, e, Status.WARNING);
+	}
 }
