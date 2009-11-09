d314bc50cc332b055813c4d5521aced0a57d16b1
claaswilke <claaswilke@gmx.net>
2009-11-09 23:19:20 +0000

    Adapted the structure of the JUnit vocabulary to the new ContractVocabulary interface.

--- treaty-eclipse-tests/treaty-eclipse-voc-junit-tests/src/net/java/treaty/eclipse/vocabulary/junit/tests/Tests.java
+++ treaty-eclipse-tests/treaty-eclipse-voc-junit-tests/src/net/java/treaty/eclipse/vocabulary/junit/tests/Tests.java
@@ -26,83 +26,116 @@ import org.eclipse.core.runtime.IExtension;
 import org.eclipse.core.runtime.IExtensionRegistry;
 import org.junit.*;
 import org.osgi.framework.Bundle;
+
 /**
  * Tests for Java vocabulary.
+ * 
  * @author Jens Dietrich
  */
 public class Tests {
+
 	private JUnitVocabulary VOC = null;
 	private EclipsePlugin plugin = null;
 	private EclipseConnector connector = null;
+
 	@Before
 	public void setUp() throws Exception {
-		VOC = new JUnitVocabulary();
+
+		VOC = JUnitVocabulary.INSTANCE;
 		String id = Activator.PLUGIN_ID;
 		Bundle bundle = org.eclipse.core.runtime.Platform.getBundle(id);
 		plugin = new EclipsePlugin(bundle);
-		IExtensionRegistry registry = org.eclipse.core.runtime.Platform.getExtensionRegistry();
-		IExtension x = registry.getExtension("net.java.treaty.eclipse.vocabulary.junit");
-		connector = new EclipseExtension(plugin,x);
+		IExtensionRegistry registry =
+				org.eclipse.core.runtime.Platform.getExtensionRegistry();
+		IExtension x =
+				registry.getExtension("net.java.treaty.eclipse.vocabulary.junit");
+		connector = new EclipseExtension(plugin, x);
 	}
 
 	@After
 	public void tearDown() throws Exception {
+
 		VOC = null;
 		plugin = null;
 		connector = null;
 	}
-	
-	private Resource getResource(String type,String clazz) throws Exception {
+
+	private Resource getResource(String type, String clazz) throws Exception {
+
 		Resource r = new Resource();
 		r.setType(new URI(type));
 		r.setName(clazz);
 		// resources must be loaded using the verifier - Java instantiable classes
 		// are loaded from another vocabulary!
-		Object value = new EclipseVerifier().load(r.getType(),r.getName(), connector);
+		Object value =
+				new EclipseVerifier().load(r.getType(), r.getName(), connector);
 		r.setValue(value);
 		return r;
 	}
-	
+
 	@Test
 	public void testTypes1() throws Exception {
-		Resource r = this.getResource(VOC.TESTCASE, DateFormatterFunctionalTests.class.getName());
+
+		Resource r =
+				this.getResource(JUnitVocabulary.TESTCASE,
+						DateFormatterFunctionalTests.class.getName());
 		ExistsCondition c = new ExistsCondition();
 		c.setResource(r);
 		VOC.check(c);
 	}
-	@Test(expected = VerificationException.class) // does not have the right constructors
+
+	@Test(expected = VerificationException.class)
+	// does not have the right constructors
 	public void testTypes2() throws Exception {
-		Resource r = this.getResource(VOC.TESTCASE, Tests.class.getName());
+
+		Resource r =
+				this.getResource(JUnitVocabulary.TESTCASE, Tests.class.getName());
 		ExistsCondition c = new ExistsCondition();
 		c.setResource(r);
 		VOC.check(c);
 	}
-	@Test(expected = VerificationException.class) // does not have the right constructors
+
+	@Test(expected = VerificationException.class)
+	// does not have the right constructors
 	public void testTypes3() throws Exception {
-		Resource r = this.getResource(VOC.TESTCASE, EmptyTestCase.class.getName());
+
+		Resource r =
+				this.getResource(JUnitVocabulary.TESTCASE, EmptyTestCase.class
+						.getName());
 		ExistsCondition c = new ExistsCondition();
 		c.setResource(r);
 		VOC.check(c);
 	}
+
 	@Test
 	public void testRelationship1() throws Exception {
-		Resource r1 = this.getResource(VOC.TESTCASE, DateFormatterFunctionalTests.class.getName());
-		Resource r2 = this.getResource(VOC.INSTANTIABLE_CLASS, ShortDateFormatter.class.getName());
+
+		Resource r1 =
+				this.getResource(JUnitVocabulary.TESTCASE,
+						DateFormatterFunctionalTests.class.getName());
+		Resource r2 =
+				this.getResource(JUnitVocabulary.INSTANTIABLE_CLASS,
+						ShortDateFormatter.class.getName());
 		RelationshipCondition rel = new RelationshipCondition();
 		rel.setResource1(r2);
 		rel.setResource2(r1);
-		rel.setRelationship(new URI(VOC.VERIFIES));
+		rel.setRelationship(new URI(JUnitVocabulary.VERIFIES));
 		VOC.check(rel);
 	}
+
 	@Test(expected = VerificationException.class)
 	public void testRelationship2() throws Exception {
-		Resource r1 = this.getResource(VOC.TESTCASE, DateFormatterFunctionalTests.class.getName());
-		Resource r2 = this.getResource(VOC.INSTANTIABLE_CLASS, TooShortDateFormatter.class.getName());
+
+		Resource r1 =
+				this.getResource(JUnitVocabulary.TESTCASE,
+						DateFormatterFunctionalTests.class.getName());
+		Resource r2 =
+				this.getResource(JUnitVocabulary.INSTANTIABLE_CLASS,
+						TooShortDateFormatter.class.getName());
 		RelationshipCondition rel = new RelationshipCondition();
 		rel.setResource1(r2);
 		rel.setResource2(r1);
-		rel.setRelationship(new URI(VOC.VERIFIES));
+		rel.setRelationship(new URI(JUnitVocabulary.VERIFIES));
 		VOC.check(rel);
 	}
-	
 }
