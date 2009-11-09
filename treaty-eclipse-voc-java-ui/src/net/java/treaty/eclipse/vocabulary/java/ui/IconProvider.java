9a725798896bc4e889c80a5531154ecdad500c9c
claaswilke <claaswilke@gmx.net>
2009-11-09 23:28:52 +0000

    Removed dependency from JavaVocabulary UI to deprecated JavaVocabulary.

--- treaty-eclipse-voc-java-ui/src/net/java/treaty/eclipse/vocabulary/java/ui/IconProvider.java
+++ treaty-eclipse-voc-java-ui/src/net/java/treaty/eclipse/vocabulary/java/ui/IconProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008 Jens Dietrich
+ * Copyright (C) 2009 Jens Dietrich
  * Licensed under the Apache License, Version 2.0 (the "License"); 
  * you may not use this file except in compliance with the License. 
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 
@@ -11,41 +11,55 @@ package net.java.treaty.eclipse.vocabulary.java.ui;
 
 import java.net.URI;
 
-import net.java.treaty.eclipse.vocabulary.java.JavaVocabulary;
+import net.java.treaty.vocabulary.builtins.java.JavaVocabulary;
+
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 /**
- * Provider for icons representing types in user interfaces.
- * Extensions provide concrete subclasses.
+ * <p>
+ * Provider for icons representing types in user interfaces. Extensions provide
+ * concrete subclasses.
+ * </p>
+ * 
  * @author Jens Dietrich
  */
-
 public class IconProvider extends net.java.treaty.eclipse.views.IconProvider {
-	
+
 	/**
 	 * Get the icon
-	 * @param type the type
-	 * @param isVariable whether the resource is a variable
+	 * 
+	 * @param type
+	 *          the type
+	 * @param isVariable
+	 *          whether the resource is a variable
 	 * @return
 	 */
-	public Image getIcon(URI type,boolean isVariable) {
+	public Image getIcon(URI type, boolean isVariable) {
+
 		String t = type.toString();
 		ImageDescriptor d = null;
+
 		if (JavaVocabulary.INSTANTIABLE_CLASS.equals(t)) {
-			d = isVariable?getImageDescriptor("icons/class_var.gif"):getImageDescriptor("icons/class.gif");
+			d =
+					isVariable ? getImageDescriptor("icons/class_var.gif")
+							: getImageDescriptor("icons/class.gif");
 		}
 		else if (JavaVocabulary.ABSTRACT_TYPE.equals(t)) {
-			d = isVariable?getImageDescriptor("icons/interface_var.gif"):getImageDescriptor("icons/interface.gif");
+			d =
+					isVariable ? getImageDescriptor("icons/interface_var.gif")
+							: getImageDescriptor("icons/interface.gif");
 		}
-		if (d!=null) {
+		if (d != null) {
 			return d.createImage();
 		}
 		return null;
 	}
-	
+
 	private ImageDescriptor getImageDescriptor(String path) {
-		return AbstractUIPlugin.imageDescriptorFromPlugin(Activator.PLUGIN_ID, path); 
+
+		return AbstractUIPlugin
+				.imageDescriptorFromPlugin(Activator.PLUGIN_ID, path);
 	}
 }
