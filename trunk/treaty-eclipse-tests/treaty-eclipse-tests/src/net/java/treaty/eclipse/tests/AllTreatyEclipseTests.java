35ea982648bca32da78fa40c804e4cf9074cd059
claaswilke@gmx.net <claaswilke@gmx.net@a4919d77-c54b-0410-8eaf-491ad53b66f1>
2009-11-10 20:59:44 +0000

    Added Treaty Eclipse Test Suite Plug-in to trunk.

---
+++ trunk/treaty-eclipse-tests/treaty-eclipse-tests/src/net/java/treaty/eclipse/tests/AllTreatyEclipseTests.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2009 Jens Dietrich
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License 
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and limitations under the License. 
+ */
+
+package net.java.treaty.eclipse.tests;
+
+import net.java.treaty.eclipse.tests.contractregistry.AllContractRegistryTests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * <p>
+ * Provides a JUnit Test Suite containing all JUnit test cases of the Treaty
+ * Eclipse implementation.
+ * </p>
+ * 
+ * @author Claas Wilke
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses( { AllContractRegistryTests.class })
+public class AllTreatyEclipseTests {
+	/*
+	 * This class remains completely empty, being used only as a holder for the
+	 * above annotations.
+	 */
+}
