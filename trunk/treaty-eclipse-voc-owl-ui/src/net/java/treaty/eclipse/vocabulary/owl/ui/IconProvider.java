4fa42290f442b6b2a318fb35a71997d0c54fb36a
claaswilke@gmx.net <claaswilke@gmx.net@a4919d77-c54b-0410-8eaf-491ad53b66f1>
2009-11-13 00:14:17 +0000

    Added UI plug-in for built-in OWL Vocabulary to trunk.

---
+++ trunk/treaty-eclipse-voc-owl-ui/src/net/java/treaty/eclipse/vocabulary/owl/ui/IconProvider.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2009 Jens Dietrich
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License 
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and limitations under the License. 
+ */
+package net.java.treaty.eclipse.vocabulary.owl.ui;
+
+import java.net.URI;
+
+import net.java.treaty.vocabulary.builtins.owl.OWLVocabulary;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+/**
+ * <p>
+ * Provider for icons representing types in user interfaces. Extensions provide
+ * concrete subclasses.
+ * </p>
+ * 
+ * @author Claas Wilke
+ */
+public class IconProvider extends net.java.treaty.eclipse.views.IconProvider {
+
+	/*
+	 * (non-Javadoc)
+	 * @see net.java.treaty.eclipse.views.IconProvider#getIcon(java.net.URI,
+	 * boolean)
+	 */
+	public org.eclipse.swt.graphics.Image getIcon(URI type, boolean isVariable) {
+
+		String typeName = type.toString();
+		ImageDescriptor imageDescriptor = null;
+
+		if (OWLVocabulary.TYPE_NAME_ONTOLOGY.equals(typeName)) {
+
+			if (isVariable) {
+				imageDescriptor = getImageDescriptor("icons/ontology_var.gif");
+			}
+
+			else {
+				imageDescriptor = getImageDescriptor("icons/ontology.gif");
+			}
+		}
+		// no else.
+
+		if (imageDescriptor != null) {
+			return imageDescriptor.createImage();
+		}
+		return null;
+	}
+
+	/**
+	 * <p>
+	 * Returns an {@link ImageDescriptor} for a given path.
+	 * </p>
+	 * 
+	 * @param path
+	 *          The path for that an {@link ImageDescriptor} shall be returned.
+	 * @return An {@link ImageDescriptor} for a given path.
+	 */
+	private ImageDescriptor getImageDescriptor(String path) {
+
+		return AbstractUIPlugin
+				.imageDescriptorFromPlugin(Activator.PLUGIN_ID, path);
+	}
+}
