891d5e87adc5af702088d3bccdea2cbcd9498ab2
claaswilke@gmx.net <claaswilke@gmx.net@a4919d77-c54b-0410-8eaf-491ad53b66f1>
2009-11-16 01:00:34 +0000

    Adapted the CSVExporter to the changed interface and package dependencies.

--- trunk/treaty-eclipse-exporter-csv/src/net/java/treaty/eclipse/exporter/csv/CSVExporter.java
+++ trunk/treaty-eclipse-exporter-csv/src/net/java/treaty/eclipse/exporter/csv/CSVExporter.java
@@ -16,141 +16,173 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.Writer;
-import java.util.*;
-import org.apache.velocity.Template;
-import org.apache.velocity.VelocityContext;
-import org.apache.velocity.app.VelocityEngine;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import net.java.treaty.AbstractCondition;
 import net.java.treaty.AbstractContractVisitor;
-import net.java.treaty.AggregatedContract;
 import net.java.treaty.Contract;
 import net.java.treaty.ContractVisitor;
 import net.java.treaty.ExistsCondition;
 import net.java.treaty.PropertyCondition;
 import net.java.treaty.RelationshipCondition;
 import net.java.treaty.Resource;
-import net.java.treaty.SimpleContract;
 import net.java.treaty.VerificationResult;
 import net.java.treaty.eclipse.Constants;
-import net.java.treaty.eclipse.Exporter;
+import net.java.treaty.eclipse.exporter.Exporter;
+
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
 
 /**
+ * <p>
  * Exports results into spreadsheets.
+ * </p>
+ * 
  * @author Jens Dietrich
  */
-
 public class CSVExporter extends Exporter {
-	
-	private int exceptionCounter = 1; 
-	
+
+	/** The location of the velocity template used for export. */
+	private static final String VELOCITY_TEMPLATE_LOCATION =
+			"net/java/treaty/eclipse/exporter/csv/csv.vm";
+
+	private int exceptionCounter = 1;
+
 	@Override
 	public String getName() {
+
 		return "export verification results to CSV (spreadsheet)";
 	}
 
 	@Override
-	public synchronized void export(Collection<Contract> contracts,File folder) throws IOException {
-		try{
-			exceptionCounter=1; // reset counter!
-			
+	public synchronized void export(Collection<Contract> contracts, File folder)
+			throws IOException {
+
+		try {
+			exceptionCounter = 1; // reset counter!
+
 			// group contract instances by contract def
-			Map<String,List<ContractData>> contractsByXP = new HashMap<String,List<ContractData>>(); 
-			Map<String,List<String>> atomicConditions = new HashMap<String,List<String>>();
-			Map<String,List<String>> variableNames = new HashMap<String,List<String>>();
-			
-			for (Contract c:contracts) {
-				String xpId = c.getConsumer()==null?"anonymous_xp":c.getConsumer().getId();
+			Map<String, List<ContractData>> contractsByXP =
+					new HashMap<String, List<ContractData>>();
+			Map<String, List<String>> atomicConditions =
+					new HashMap<String, List<String>>();
+			Map<String, List<String>> variableNames =
+					new HashMap<String, List<String>>();
+
+			for (Contract c : contracts) {
+				String xpId =
+						c.getConsumer() == null ? "anonymous_xp" : c.getConsumer().getId();
 				List<ContractData> instances = contractsByXP.get(xpId);
-				if (instances==null) {
+				if (instances == null) {
 					instances = new ArrayList<ContractData>();
 					contractsByXP.put(xpId, instances);
-					atomicConditions.put(xpId,this.getAtomicConditions(c));
-					variableNames.put(xpId,this.getVariables(c));
+					atomicConditions.put(xpId, this.getAtomicConditions(c));
+					variableNames.put(xpId, this.getVariables(c));
 				}
 				Contract[] parts = null;
-				if (c instanceof SimpleContract) {
-					parts = new Contract[]{c};
-				}
-				else if (c instanceof AggregatedContract) {
-					parts = ((AggregatedContract)c).getParts();
-				}
+				parts = new Contract[] { c };
 				// gather data and build contract data
-				for (int i=0;i<parts.length;i++) {
+				for (int i = 0; i < parts.length; i++) {
 					Contract p = parts[i];
 					ContractData d = new ContractData();
 					d.xp = xpId;
 					d.pluginId = c.getSupplier().getOwner().getId();
-					d.extensionPointId = c.getSupplier().getId()==null?"anonymous":c.getSupplier().getId();
-					d.isAggregated = c instanceof AggregatedContract;
+					d.extensionPointId =
+							c.getSupplier().getId() == null ? "anonymous" : c.getSupplier()
+									.getId();
 					String result = VerificationResult.UNKNOWN.toString();
-					VerificationResult vr = (VerificationResult) p.getProperty(Constants.VERIFICATION_RESULT);
-					if (vr!=null) {
+					VerificationResult vr =
+							(VerificationResult) p.getProperty(Constants.VERIFICATION_RESULT);
+					if (vr != null) {
 						result = vr.toString();
 					}
 					d.result = result;
-					d.conditionResults=this.getResults(p,folder);
-					d.bindings=this.getVariableBindings(p);
-					d.partNo=i+1;
+					d.conditionResults = this.getResults(p, folder);
+					d.bindings = this.getVariableBindings(p);
+					d.partNo = i + 1;
 					instances.add(d);
 				}
 			}
 			// load template
 			VelocityEngine ve = new VelocityEngine();
-			ve.setProperty("resource.loader","class");
-			ve.setProperty("class.resource.loader.class","org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+			ve.setProperty("resource.loader", "class");
+			ve
+					.setProperty("class.resource.loader.class",
+							"org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
 			ve.init();
-			Template template = ve.getTemplate("net/java/treaty/eclipse/defaultexporter/csv.vm");
-			
+			Template template = ve.getTemplate(VELOCITY_TEMPLATE_LOCATION);
+
 			// bind variables for each xp
-			for (String xpId:contractsByXP.keySet()) {
-				List<ContractData> instances = contractsByXP.get(xpId);				
+			for (String xpId : contractsByXP.keySet()) {
+				List<ContractData> instances = contractsByXP.get(xpId);
 				VelocityContext context = new VelocityContext();
-				context.put("contracts",instances);
-				context.put("conditions",atomicConditions.get(xpId));
-				context.put("variables",variableNames.get(xpId));
-				File file = new File(folder.getAbsolutePath()+'/'+xpId+".csv");
+				context.put("contracts", instances);
+				context.put("conditions", atomicConditions.get(xpId));
+				context.put("variables", variableNames.get(xpId));
+				File file = new File(folder.getAbsolutePath() + '/' + xpId + ".csv");
 				Writer writer = new FileWriter(file);
-				template.merge( context, writer );
+				template.merge(context, writer);
 				writer.close();
 			}
-			
-		}
-		catch (Exception x) {
+
+		} catch (Exception x) {
 			net.java.treaty.eclipse.Logger.error("Error exporting", x);
 		}
 	}
-	
-	public boolean exportToFolder(){
+
+	public boolean exportToFolder() {
+
 		return true;
 	}
+
 	public String[] getFilterExtensions() {
-		return new String[]{};
+
+		return new String[] {};
 	}
-	public String[] getFilterNames(){
-		return new String[]{};
+
+	public String[] getFilterNames() {
+
+		return new String[] {};
 	}
-	
+
 	private List<String> getExceptions(Contract contract) {
-		final List<String> exceptions = new ArrayList<String> ();
+
+		final List<String> exceptions = new ArrayList<String>();
 		ContractVisitor visitor = new AbstractContractVisitor() {
+
 			@Override
 			public boolean visit(ExistsCondition c) {
+
 				doVisit(c);
 				return true;
 			}
+
 			@Override
 			public boolean visit(PropertyCondition c) {
+
 				doVisit(c);
 				return true;
 			}
+
 			@Override
 			public boolean visit(RelationshipCondition c) {
+
 				doVisit(c);
 				return true;
 			}
+
 			private void doVisit(AbstractCondition c) {
-				Exception x = (Exception) c.getProperty(Constants.VERIFICATION_EXCEPTION);
-				if (x!=null) {
+
+				Exception x =
+						(Exception) c.getProperty(Constants.VERIFICATION_EXCEPTION);
+				if (x != null) {
 					StringWriter writer = new StringWriter();
 					x.printStackTrace(new PrintWriter(writer));
 					exceptions.add(writer.toString());
@@ -159,35 +191,50 @@ public class CSVExporter extends Exporter {
 		};
 		contract.accept(visitor);
 		return exceptions;
-	} 
+	}
+
 	private List<String> getAtomicConditions(Contract contract) {
-		// if contract is aggregated, we need to analyse only one part
-		// since all parts have the same structure
-		if (contract instanceof AggregatedContract && ((AggregatedContract)contract).getParts().length>0) {
-			contract = ((AggregatedContract)contract).getParts()[0];
-		}
-		
-		final List<String> list = new ArrayList<String> ();
+
+		final List<String> list = new ArrayList<String>();
 		ContractVisitor visitor = new AbstractContractVisitor() {
+
 			@Override
 			public boolean visit(ExistsCondition c) {
-				list.add("must exist: "+print(c.getResource()));
+
+				list.add("must exist: " + print(c.getResource()));
 				return true;
 			}
-			@Override
-			public boolean visit(PropertyCondition c) {
-				StringBuffer b = new StringBuffer();
-				b.append(print(c.getResource()));
-				b.append('.');
-				b.append(c.getProperty());
-				b.append(' ');
-				b.append(c.getOperator());
-				b.append(c.getValue());
-				list.add(b.toString());
+
+			/*
+			 * (non-Javadoc)
+			 * @seenet.java.treaty.AbstractContractVisitor#visit(net.java.treaty.
+			 * PropertyCondition)
+			 */
+			public boolean visit(PropertyCondition propertyCondition) {
+
+				StringBuffer buffer;
+				buffer = new StringBuffer();
+
+				buffer.append(print(propertyCondition.getResource()));
+				buffer.append('.');
+				/*
+				 * FIXME Claas: is this okay or can property conditions have multiple
+				 * properties?
+				 */
+				buffer.append(propertyCondition.getProperty(propertyCondition
+						.getPropertyNames().next()));
+				buffer.append(' ');
+				buffer.append(propertyCondition.getOperator());
+				buffer.append(propertyCondition.getValue());
+
+				list.add(buffer.toString());
+
 				return true;
 			}
+
 			@Override
 			public boolean visit(RelationshipCondition c) {
+
 				StringBuffer b = new StringBuffer();
 				b.append(print(c.getResource1()));
 				b.append(' ');
@@ -200,68 +247,79 @@ public class CSVExporter extends Exporter {
 		};
 		contract.accept(visitor);
 		return list;
-	} 
-	
+	}
+
 	private List<String> getVariables(Contract contract) {
-		
-		// if contract is aggregated, we need to analyse only one part
-		// since all parts have the same structure
-		if (contract instanceof AggregatedContract && ((AggregatedContract)contract).getParts().length>0) {
-			contract = ((AggregatedContract)contract).getParts()[0];
-		}
-		
-		final List<String> list = new ArrayList<String> ();
+
+		final List<String> list = new ArrayList<String>();
 		final Set<String> vars = new HashSet<String>();
 		ContractVisitor visitor = new AbstractContractVisitor() {
+
 			@Override
 			public boolean visit(ExistsCondition c) {
+
 				add(c.getResource());
 				return true;
 			}
+
 			@Override
 			public boolean visit(PropertyCondition c) {
+
 				add(c.getResource());
 				return true;
 			}
+
 			@Override
 			public boolean visit(RelationshipCondition c) {
+
 				add(c.getResource1());
 				add(c.getResource2());
 				return true;
 			}
+
 			private void add(Resource r) {
-				if (r.getRef()!=null && !vars.contains(r.getId())) {
-					list.add("variable "+print(r));
+
+				if (r.getRef() != null && !vars.contains(r.getId())) {
+					list.add("variable " + print(r));
 					vars.add(r.getId());
 				}
 			}
 		};
 		contract.accept(visitor);
 		return list;
-	} 
-	
+	}
+
 	private List<String> getVariableBindings(Contract contract) {
-		final List<String> list = new ArrayList<String> ();
+
+		final List<String> list = new ArrayList<String>();
 		final Set<String> vars = new HashSet<String>();
 		ContractVisitor visitor = new AbstractContractVisitor() {
+
 			@Override
 			public boolean visit(ExistsCondition c) {
+
 				add(c.getResource());
 				return true;
 			}
+
 			@Override
 			public boolean visit(PropertyCondition c) {
+
 				add(c.getResource());
 				return true;
 			}
+
 			@Override
 			public boolean visit(RelationshipCondition c) {
+
 				add(c.getResource1());
 				add(c.getResource2());
 				return true;
 			}
+
 			private void add(Resource r) {
-				if (r.getRef()!=null && !vars.contains(r.getId())) {
+
+				if (r.getRef() != null && !vars.contains(r.getId())) {
 					list.add(print2(r));
 					vars.add(r.getId());
 				}
@@ -269,36 +327,49 @@ public class CSVExporter extends Exporter {
 		};
 		contract.accept(visitor);
 		return list;
-	} 
-	
-	private List<String> getResults(Contract contract,final File folder) {
-		final List<String> list = new ArrayList<String> ();
+	}
+
+	private List<String> getResults(Contract contract, final File folder) {
+
+		final List<String> list = new ArrayList<String>();
 		ContractVisitor visitor = new AbstractContractVisitor() {
+
 			@Override
 			public boolean visit(ExistsCondition c) {
+
 				doVisit(c);
 				return true;
 			}
+
 			@Override
 			public boolean visit(PropertyCondition c) {
+
 				doVisit(c);
 				return true;
 			}
+
 			@Override
 			public boolean visit(RelationshipCondition c) {
+
 				doVisit(c);
 				return true;
 			}
+
 			private void doVisit(AbstractCondition c) {
+
 				list.add(getResult(c));
 			}
+
 			private String getResult(AbstractCondition c) {
-				VerificationResult r = (VerificationResult) c.getProperty(Constants.VERIFICATION_RESULT);
-				Exception x = (Exception) c.getProperty(Constants.VERIFICATION_EXCEPTION);
-				if (x!=null) {
-					String fileName = "exception"+exceptionCounter+".txt";
-					File file = new File(folder.getAbsolutePath()+'/'+fileName);
-					exceptionCounter=exceptionCounter+1;
+
+				VerificationResult r =
+						(VerificationResult) c.getProperty(Constants.VERIFICATION_RESULT);
+				Exception x =
+						(Exception) c.getProperty(Constants.VERIFICATION_EXCEPTION);
+				if (x != null) {
+					String fileName = "exception" + exceptionCounter + ".txt";
+					File file = new File(folder.getAbsolutePath() + '/' + fileName);
+					exceptionCounter = exceptionCounter + 1;
 					Writer writer;
 					try {
 						writer = new FileWriter(file);
@@ -310,44 +381,48 @@ public class CSVExporter extends Exporter {
 
 					return fileName;
 				}
-				else { 
-					return r==null?"null":r.toString();
+				else {
+					return r == null ? "null" : r.toString();
 				}
 			}
 		};
 		contract.accept(visitor);
 		return list;
-	} 
-	
-	private String printShort(String s) {
-		String NAMESPACE = "http://www.treaty.org/";
-		if (s.startsWith(NAMESPACE)) return s.substring(NAMESPACE.length());
-		else return s;
 	}
+
+	private String printShort(String s) {
+
+		String NAMESPACE = "http://www.treaty.org/";
+		if (s.startsWith(NAMESPACE))
+			return s.substring(NAMESPACE.length());
+		else
+			return s;
+	}
+
 	private String print(Resource r) {
+
 		StringBuffer b = new StringBuffer();
-		if (r.getRef()!=null) {
+		if (r.getRef() != null) {
 			b.append('?');
 			b.append(r.getRef());
 		}
-		else if (r.getName()!=null) {
+		else if (r.getName() != null) {
 			b.append(r.getName());
 		}
 		/*
-		b.append('[');
-		b.append(printShort(r.getType().toString()));
-		b.append(']');
-		*/
+		 * b.append('['); b.append(printShort(r.getType().toString()));
+		 * b.append(']');
+		 */
 		return b.toString();
 	}
+
 	private String print2(Resource r) {
-		if (r.getRef()!=null && r.getName()!=null) {
+
+		if (r.getRef() != null && r.getName() != null) {
 			return r.getName();
 		}
 		else {
 			return "?";
 		}
 	}
-
-	
 }
