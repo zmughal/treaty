5d545cb30d0b96fe87d15015c087582bdd9d3b5e
claaswilke <claaswilke@gmx.net>
2009-12-08 00:23:54 +0000

    Adapted test cases to changed interfaces.

--- treaty-eclipse-tests/treaty-eclipse-tests/src/net/java/treaty/eclipse/tests/verification/SystemExampleTests.java
+++ treaty-eclipse-tests/treaty-eclipse-tests/src/net/java/treaty/eclipse/tests/verification/SystemExampleTests.java
@@ -149,6 +149,30 @@ public class SystemExampleTests {
 			 * here.
 			 */
 		}
+
+		/*
+		 * (non-Javadoc)
+		 * @seenet.java.treaty.eclipse.verification.VerificationJobListener#
+		 * verificationOfContractFailed(net.java.treaty.Contract,
+		 * net.java.treaty.VerificationReport)
+		 */
+		public void verificationOfContractFailed(Contract contract,
+				VerificationReport verificationReport) {
+
+			/* Do nothing. */
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * @seenet.java.treaty.eclipse.verification.VerificationJobListener#
+		 * verificationOfContractSucceeded(net.java.treaty.Contract,
+		 * net.java.treaty.VerificationReport)
+		 */
+		public void verificationOfContractSucceeded(Contract contract,
+				VerificationReport verificationReport) {
+
+			/* Do nothing. */
+		}
 	}
 
 	/**
