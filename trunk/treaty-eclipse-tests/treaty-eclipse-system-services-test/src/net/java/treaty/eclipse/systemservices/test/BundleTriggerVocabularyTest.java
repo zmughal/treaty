c5a894d631279ea4e195ffb2fb1634098c1d3810
claaswilke@gmx.net <claaswilke@gmx.net@a4919d77-c54b-0410-8eaf-491ad53b66f1>
2009-12-09 22:15:38 +0000

    Adapted BundleTriggerVocabulary to new BundleVocabulary interface and introduced super trigger BundleEvent for all other triggers.

--- trunk/treaty-eclipse-tests/treaty-eclipse-system-services-test/src/net/java/treaty/eclipse/systemservices/test/BundleTriggerVocabularyTest.java
+++ trunk/treaty-eclipse-tests/treaty-eclipse-system-services-test/src/net/java/treaty/eclipse/systemservices/test/BundleTriggerVocabularyTest.java
@@ -10,18 +10,20 @@
 
 package net.java.treaty.eclipse.systemservices.test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Collections;
 import java.util.Set;
 
 import net.java.treaty.Contract;
 import net.java.treaty.Role;
 import net.java.treaty.TreatyException;
-import net.java.treaty.contractregistry.ContractRegistry.UpdateType;
 import net.java.treaty.eclipse.EclipseExtensionPoint;
 import net.java.treaty.eclipse.contractregistry.EclipseAdapterFactory;
 import net.java.treaty.eclipse.contractregistry.EclipseConnectorAdaptationException;
@@ -45,7 +47,7 @@ import org.osgi.framework.BundleEvent;
  * 
  * @author Claas Wilke
  */
-public class BundleTriggerTest {
+public class BundleTriggerVocabularyTest {
 
 	/** The ID of a {@link Bundle} used during testing. */
 	private static final String CONSUMER_BUNDLE_1_ID =
@@ -56,11 +58,27 @@ public class BundleTriggerTest {
 			CONSUMER_BUNDLE_1_ID + ".xp1";
 
 	/** The ID of a {@link Bundle} used during testing. */
+	private static final String CONSUMER_BUNDLE_2_ID =
+			"net.java.treaty.test.consumer2";
+
+	/** The ID of an {@link IExtensionPoint} used during testing. */
+	private static final String CONSUMER_BUNDLE_2_EXTENSION_POINT1_ID =
+			CONSUMER_BUNDLE_2_ID + ".xp1";
+
+	/** The ID of a {@link Bundle} used during testing. */
 	private static final String PROVIDER_BUNDLE_1_ID =
 			"net.java.treaty.test.provider1";
 
 	/** The ID of an {@link IExtension} used during testing. */
 	private static final String PROVIDER_BUNDLE_1_EXTENSION_1_ID =
+			PROVIDER_BUNDLE_1_ID + ".x1";
+
+	/** The ID of a {@link Bundle} used during testing. */
+	private static final String PROVIDER_BUNDLE_2_ID =
+			"net.java.treaty.test.provider1";
+
+	/** The ID of an {@link IExtension} used during testing. */
+	private static final String PROVIDER_BUNDLE_2_EXTENSION_1_ID =
 			PROVIDER_BUNDLE_1_ID + ".x1";
 
 	/** The maximum loops to wait for a verification result. */
@@ -72,6 +90,9 @@ public class BundleTriggerTest {
 	/** A {@link Bundle} used during testing. */
 	private static Bundle consumerBundle1;
 
+	/** A {@link Bundle} used during testing. */
+	private static Bundle consumerBundle2;
+
 	/** The {@link IExtensionRegistry} used during testing. */
 	private static IExtensionRegistry extensionRegistry =
 			org.eclipse.core.runtime.Platform.getExtensionRegistry();
@@ -79,20 +100,26 @@ public class BundleTriggerTest {
 	/** A {@link Bundle} used during testing. */
 	private static Bundle providerBundle1;
 
+	/** A {@link Bundle} used during testing. */
+	private static Bundle providerBundle2;
+
 	/** An {@link IExtensionPoint} used during testing. */
 	private static IExtensionPoint extensionPoint1;
+
+	/** An {@link IExtensionPoint} used during testing. */
+	private static IExtensionPoint extensionPoint2;
 
 	/** An {@link EclipseExtensionPoint} used during testing. */
 	private static EclipseExtensionPoint eclipseExtensionPoint1;
 
+	/** An {@link EclipseExtensionPoint} used during testing. */
+	private static EclipseExtensionPoint eclipseExtensionPoint2;
+
 	/** An {@link IExtension} used during testing. */
 	private static IExtension extension1;
 
-	/** A {@link Contract} used during testing. */
-	private static Contract contract1;
-
-	/** A {@link Contract} used during testing. */
-	private static Contract contract2;
+	/** An {@link IExtension} used during testing. */
+	private static IExtension extension2;
 
 	/** Indicates whether or not an expected event has been fired. */
 	private Boolean wasRightEventFired;
@@ -111,10 +138,17 @@ public class BundleTriggerTest {
 		consumerBundle1 =
 				org.eclipse.core.runtime.Platform.getBundle(CONSUMER_BUNDLE_1_ID);
 
+		consumerBundle2 =
+				org.eclipse.core.runtime.Platform.getBundle(CONSUMER_BUNDLE_2_ID);
+
 		providerBundle1 =
 				org.eclipse.core.runtime.Platform.getBundle(PROVIDER_BUNDLE_1_ID);
 
-		if (consumerBundle1 == null || providerBundle1 == null) {
+		providerBundle2 =
+				org.eclipse.core.runtime.Platform.getBundle(PROVIDER_BUNDLE_2_ID);
+
+		if (consumerBundle1 == null || providerBundle1 == null
+				|| consumerBundle2 == null || providerBundle2 == null) {
 
 			String msg;
 			msg = "Cannot find all required Bundles for testing. Testing failed.";
@@ -127,7 +161,11 @@ public class BundleTriggerTest {
 				extensionRegistry
 						.getExtensionPoint(CONSUMER_BUNDLE_1_EXTENSION_POINT1_ID);
 
-		if (extensionPoint1 == null) {
+		extensionPoint2 =
+				extensionRegistry
+						.getExtensionPoint(CONSUMER_BUNDLE_2_EXTENSION_POINT1_ID);
+
+		if (extensionPoint1 == null || extensionPoint2 == null) {
 
 			String msg;
 			msg =
@@ -140,7 +178,10 @@ public class BundleTriggerTest {
 		extension1 =
 				extensionRegistry.getExtension(PROVIDER_BUNDLE_1_EXTENSION_1_ID);
 
-		if (extension1 == null) {
+		extension2 =
+				extensionRegistry.getExtension(PROVIDER_BUNDLE_2_EXTENSION_1_ID);
+
+		if (extension1 == null || extension2 == null) {
 
 			String msg;
 			msg = "Cannot find all required IExtensions for testing. Testing failed.";
@@ -153,6 +194,10 @@ public class BundleTriggerTest {
 			eclipseExtensionPoint1 =
 					EclipseAdapterFactory.getInstance().createExtensionPoint(
 							extensionPoint1);
+
+			eclipseExtensionPoint2 =
+					EclipseAdapterFactory.getInstance().createExtensionPoint(
+							extensionPoint2);
 		}
 
 		catch (EclipseConnectorAdaptationException e) {
@@ -164,23 +209,114 @@ public class BundleTriggerTest {
 
 	/**
 	 * <p>
-	 * Removes newly added {@link Contract}s from the
-	 * {@link EclipseContractRegistry} to avoid side effects.
+	 * Tests the method {@link BundleTriggerVocabulary#getSubTriggers(URI)}.
 	 * </p>
 	 * 
-	 * @throws TreatyException
+	 * @throws URISyntaxException
 	 */
-	public void tearDown() throws TreatyException {
+	@Test
+	public void testSubTriggers01() throws TreatyException, URISyntaxException {
 
-		/* Remove the contracts from the registry. */
-		EclipseContractRegistry.getInstance().updateContract(
-				UpdateType.REMOVE_CONTRACT, contract1, eclipseExtensionPoint1,
-				Role.CONSUMER);
+		BundleTriggerVocabulary bundleTriggerVocabulary;
+		bundleTriggerVocabulary = new BundleTriggerVocabulary();
 
-		/* Remove the contracts from the registry. */
-		EclipseContractRegistry.getInstance().updateContract(
-				UpdateType.REMOVE_CONTRACT, contract2, eclipseExtensionPoint1,
-				Role.CONSUMER);
+		Set<URI> subTriggers;
+		subTriggers =
+				bundleTriggerVocabulary.getSubTriggers(new URI(
+						BundleTriggerVocabulary.TRIGGER_TYPE_BUNDLE_EVENT));
+
+		assertNotNull(subTriggers);
+		assertEquals(10, subTriggers.size());
+
+		assertTrue(subTriggers.contains(new URI(
+				BundleTriggerVocabulary.TRIGGER_TYPE_BUNDLE_INSTALLED)));
+		assertTrue(subTriggers.contains(new URI(
+				BundleTriggerVocabulary.TRIGGER_TYPE_BUNDLE_LAZY_ACTIVATION)));
+		assertTrue(subTriggers.contains(new URI(
+				BundleTriggerVocabulary.TRIGGER_TYPE_BUNDLE_RESOLVED)));
+		assertTrue(subTriggers.contains(new URI(
+				BundleTriggerVocabulary.TRIGGER_TYPE_BUNDLE_STARTED)));
+		assertTrue(subTriggers.contains(new URI(
+				BundleTriggerVocabulary.TRIGGER_TYPE_BUNDLE_STARTING)));
+		assertTrue(subTriggers.contains(new URI(
+				BundleTriggerVocabulary.TRIGGER_TYPE_BUNDLE_STOPPED)));
+		assertTrue(subTriggers.contains(new URI(
+				BundleTriggerVocabulary.TRIGGER_TYPE_BUNDLE_STOPPING)));
+		assertTrue(subTriggers.contains(new URI(
+				BundleTriggerVocabulary.TRIGGER_TYPE_BUNDLE_UNINSTALLED)));
+		assertTrue(subTriggers.contains(new URI(
+				BundleTriggerVocabulary.TRIGGER_TYPE_BUNDLE_UNRESOLVED)));
+		assertTrue(subTriggers.contains(new URI(
+				BundleTriggerVocabulary.TRIGGER_TYPE_BUNDLE_UPDATED)));
+	}
+
+	/**
+	 * <p>
+	 * Tests the method {@link BundleTriggerVocabulary#getSubTriggers(URI)}.
+	 * </p>
+	 * 
+	 * @throws URISyntaxException
+	 */
+	@Test
+	public void testSubTriggers02() throws TreatyException, URISyntaxException {
+
+		BundleTriggerVocabulary bundleTriggerVocabulary;
+		bundleTriggerVocabulary = new BundleTriggerVocabulary();
+
+		Set<URI> subTriggers;
+		subTriggers =
+				bundleTriggerVocabulary.getSubTriggers(new URI(
+						BundleTriggerVocabulary.TRIGGER_TYPE_BUNDLE_INSTALLED));
+
+		assertNotNull(subTriggers);
+		assertEquals(0, subTriggers.size());
+	}
+
+	/**
+	 * <p>
+	 * Tests the method {@link BundleTriggerVocabulary#getSubTriggers(URI)}.
+	 * </p>
+	 * 
+	 * @throws URISyntaxException
+	 */
+	@Test
+	public void testSuperTriggers01() throws TreatyException, URISyntaxException {
+
+		BundleTriggerVocabulary bundleTriggerVocabulary;
+		bundleTriggerVocabulary = new BundleTriggerVocabulary();
+
+		Set<URI> superTriggers;
+		superTriggers =
+				bundleTriggerVocabulary.getSuperTriggers(new URI(
+						BundleTriggerVocabulary.TRIGGER_TYPE_BUNDLE_INSTALLED));
+
+		assertNotNull(superTriggers);
+		assertEquals(1, superTriggers.size());
+
+		assertTrue(superTriggers.contains(new URI(
+				BundleTriggerVocabulary.TRIGGER_TYPE_BUNDLE_EVENT)));
+	}
+
+	/**
+	 * <p>
+	 * Tests the method {@link BundleTriggerVocabulary#getSubTriggers(URI)}.
+	 * </p>
+	 * 
+	 * @throws URISyntaxException
+	 */
+	@Test
+	public void testSuperTriggers02() throws TreatyException, URISyntaxException {
+
+		BundleTriggerVocabulary bundleTriggerVocabulary;
+		bundleTriggerVocabulary = new BundleTriggerVocabulary();
+
+		Set<URI> superTriggers;
+		superTriggers =
+				bundleTriggerVocabulary.getSuperTriggers(new URI(
+						BundleTriggerVocabulary.TRIGGER_TYPE_BUNDLE_EVENT));
+
+		assertNotNull(superTriggers);
+		assertEquals(0, superTriggers.size());
 	}
 
 	/**
@@ -368,5 +504,99 @@ public class BundleTriggerTest {
 		// end while.
 
 		assertNull(wasRightEventFired);
+	}
+
+	/**
+	 * <p>
+	 * Tests that a {@link BundleEvent} causes the right trigger for verification
+	 * if the trigger not defined but a super trigger of the trigger is defined in
+	 * a contract of the {@link Bundle}.
+	 * </p>
+	 */
+	@Test
+	public void testBundleEvent03() {
+
+		/* Wait for the ContractRegistry getting ready. */
+		int loops;
+		loops = 0;
+
+		Set<Contract> contracts;
+		contracts = Collections.emptySet();
+
+		while (contracts.size() == 0) {
+
+			contracts =
+					EclipseContractRegistry.getInstance().getContracts(
+							eclipseExtensionPoint2, Role.CONSUMER);
+
+			/* Wait some time to improve CPU performance. */
+			try {
+				Thread.sleep(WAIT_PAUSE_LENGTH);
+			}
+
+			catch (InterruptedException e) {
+				fail("Cannot ensure that testcase behaves determinisitc. Test failed.");
+			}
+			// end catch.
+
+			loops++;
+			if (loops > WAIT_TOTAL_LOOPS) {
+				fail("Required Contract for test not available.");
+			}
+			// no else.
+		}
+		// end while.
+
+		BundleTriggerVocabulary bundleTriggerVocabulary;
+		bundleTriggerVocabulary = new BundleTriggerVocabulary();
+
+		this.wasRightEventFired = null;
+
+		/* Register a listener. */
+		bundleTriggerVocabulary.addEventListener(new EventListener() {
+
+			/*
+			 * (non-Javadoc)
+			 * @see net.java.treaty.trigger.EventListener#update(java.net.URI,
+			 * java.util.Set)
+			 */
+			public void update(URI triggerType, Set<Contract> contracts) {
+
+				wasRightEventFired =
+						BundleTriggerVocabulary.TRIGGER_TYPE_BUNDLE_STARTED
+								.equals(triggerType.toString());
+			}
+		});
+
+		/* Cause Event. */
+		BundleEvent bundleEvent;
+		bundleEvent = new BundleEvent(BundleEvent.STARTED, consumerBundle2);
+
+		bundleTriggerVocabulary.bundleChanged(bundleEvent);
+
+		/* Wait for result. */
+		loops = 0;
+
+		while (wasRightEventFired == null) {
+
+			/* Wait some time to improve CPU performance. */
+			try {
+				Thread.sleep(WAIT_PAUSE_LENGTH);
+			}
+
+			catch (InterruptedException e) {
+				fail("Cannot ensure that testcase behaves determinisitc. Test failed.");
+			}
+			// end catch.
+
+			loops++;
+			if (loops > WAIT_TOTAL_LOOPS) {
+				fail("Triggered event was not available.");
+			}
+			// no else.
+		}
+		// end while.
+
+		assertTrue(wasRightEventFired);
 	}
 }
