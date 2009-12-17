7c2648b4e69b3e9e23241715a9112525158b0afe
claaswilke <claaswilke@gmx.net>
2009-12-17 00:32:06 +0000

    Tagged 2.1.0 release.

---
+++ treaty-eclipse-voc-junit/src/net/java/treaty/eclipse/vocabulary/junit/Activator.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2009 Jens Dietrich
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License 
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and limitations under the License. 
+ */
+
+package net.java.treaty.eclipse.vocabulary.junit;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * <p>
+ * The activator class controls the plug-in life cycle.
+ * </p>
+ * 
+ * @author Claas Wilke
+ */
+public class Activator extends Plugin {
+
+	/** The plug-in ID. */
+	public static final String PLUGIN_ID =
+			"net.java.treaty.eclipse.vocabulary.junit";
+
+	/** The shared instance. */
+	private static Activator plugin;
+
+	/**
+	 * <p>
+	 * The constructor.
+	 * </p>
+	 */
+	public Activator() {
+
+		/* Remains empty. */
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
+	 */
+	public void start(BundleContext context) throws Exception {
+
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
+	 */
+	public void stop(BundleContext context) throws Exception {
+
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * <p>
+	 * Returns the shared instance.
+	 * </p>
+	 * 
+	 * @return The shared instance.
+	 */
+	public static Activator getDefault() {
+
+		return plugin;
+	}
+}
